8132198b2c9b5bba23bfe0fd93f3e0d36bc8d974
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-03-05 22:01:02 +0600

    feat: add solution for typical_array_problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (typeof array !== 'undefined' && array.length>0) {
+    let a=array[0];
+  for (i=1; i<array.length; i++) {
+    if (array[i] < a) {
+      a=array[i];
+    } 
+  }
+    return a;
+} else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length>0) {
+    let a=array[0];
+  for (i=1; i<array.length; i++) {
+    if (array[i] > a) {
+      a=array[i];
+    }
+  }
+  return a;
+} else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length>0) {
+  let count=0;
+  for (i=0; i<array.length; i++) {
+    count=count + array[i];
+  }
+  return count/array.length;
+} else return 0;
 }
+
+/*
+function min(array) {
+  let a=array[0];
+  for (i=1; i<array.length; i++) {
+    if (array[i] < a) {
+      a=array[i];
+    }
+  }
+  return a;
+}
+
+function max(array) {
+  let a=array[0];
+  for (i=1; i<array.length; i++) {
+    if (array[i] > a) {
+      a=array[i];
+    }
+  }
+  return a;
+}
+*/
